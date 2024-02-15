e8c8db9f1991f44f9db618aec0e9ee4a82e1915a
nara04040 <nara04040@naver.com>
2024-02-15 21:58:46 +0900

    style: TODOLIST Page 초기 스타일 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,88 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <div className="container mx-auto p-8">
+          <header className="flex justify-between items-center py-4 px-6 bg-main-background text-white">
+            <h1 className="text-base text-black font-bold">Hows Your Day</h1>
+            <div className="space-x-4">
+              <button className="p-2 rounded bg-purple-500 hover:bg-purple-600 ">
+                <span className="text-sm text-white font-medium">+ Create</span>
+              </button>
+              <button className="p-2 rounded bg-purple-500 hover:bg-purple-600 ">
+                <span className="text-sm text-black font-medium">Search 아이콘</span>
+              </button>
+              <button className="p-2 rounded bg-purple-500 hover:bg-purple-600">
+                <span className="text-sm text-black font-medium">Notifications 아이콘</span>
+              </button>
+              <button className="p-2 rounded bg-purple-500 hover:bg-purple-600">
+                <span className="text-sm text-black font-medium">Setting 아이콘</span>
+              </button>
+              <button className="p-2 rounded bg-purple-500 hover:bg-purple-600">
+                <span className="text-sm text-black font-medium">User 아이콘</span>
+              </button>
+            </div>
+          </header>
+
+          <main className="mt-8">
+            <div className="flex">
+              <aside className="w-1/4">
+                <nav>
+                  <ul className="space-y-2">
+                    <li>
+                      <a href="#" className="block p-4 font-bold bg-main-color rounded">
+                        TODOLIST
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="block p-4 text-black font-bold hover:bg-purple-400 rounded">
+                        Calendar
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </aside>
+              <section className="w-3/4 ml-8">
+                <div className="bg-white px-4 py-5 rounded shadow-lg">
+                  <div className="py-3 text-black font-bold text-xl mb-4 border-b-2">TodoList</div>
+                  <div className="mb-4 pl-10">
+                    <form id="addTodoForm">
+                      <div className="flex items-center w-2/4">
+                        <div className=" mr-20">
+                          <label htmlFor="date" className="block text-sm font-semibold">
+                            Date
+                          </label>
+                          <input type="date" id="date" name="date" className="p-2 border rounded" />
+                        </div>
+                        <div>
+                          <label htmlFor="time" className="block text-sm font-semibold">
+                            Time
+                          </label>
+                          <input type="time" id="time" name="time" className="p-2 border rounded" />
+                        </div>
+                      </div>
+                      <div className="mb-2">
+                        <label htmlFor="todo" className="block text-sm font-semibold">
+                          Add Todo
+                        </label>
+                        <ul className="space-y-2 mb-5" id="todoList">
+                          <li className="p-3 bg-gray-100 text-black rounded">Sample Task 1</li>
+                          <li className="p-3 bg-gray-100 text-black rounded">Sample Task 2</li>
+                        </ul>
+                        <input type="text" id="todo" name="todo" className="w-2/4 p-2 border-none rounded" placeholder="New task" />
+                        <button type="submit" className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded">
+                          Add
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </main>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      ;
+    </>
   );
 }
